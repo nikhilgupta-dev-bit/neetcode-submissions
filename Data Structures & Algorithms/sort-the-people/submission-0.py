a9bf9f96e39f1d83a8859e953f32622054d7dabe
Nikhil Gupta <nikhilunofficial123@gmail.com>
2026-07-29 22:05:49 +0530

    Add: sort-the-people - submission-0

---
+++ Data Structures & Algorithms/sort-the-people/submission-0.py
@@ -0,0 +1,12 @@
+class Solution:
+    def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
+        pairs=list(zip(heights,names))
+        pairs.sort(reverse=True)
+        return [name for _ , name in pairs]
+
+
+
+
+
+
+
